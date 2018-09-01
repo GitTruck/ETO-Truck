d8523851632c2320026103ae4b30a9b7fd5a49ce
GitTruck <42881554+GitTruck@users.noreply.github.com>
2018-09-01 11:23:57 +0930

    implement basic request router

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'Vroom' . PHP_EOL;
+$requestPath = $_SERVER['REQUEST_URI'];
+
+switch($requestPath) {
+  case '/':
+  case 'home':
+    echo 'Vroom' . PHP_EOL; 
+    break;
+    
+  case '/database':
+    echo 'You have entered the database.' . PHP_EOL;
+    break;
+    
+  default:
+    echo 'Unkown Page' . PHP_EOL;
+}
