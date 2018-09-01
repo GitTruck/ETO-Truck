4a1758f1af0eccd6cb47bf391cfcb55c6ba7202f
GitTruck <42881554+GitTruck@users.noreply.github.com>
2018-09-01 11:47:37 +0930

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ $requestPath = $_SERVER['REQUEST_URI'];
 
 switch($requestPath) {
   case '/':
-  case 'home':
     echo 'Vroom' . PHP_EOL; 
     break;
     
@@ -13,5 +12,23 @@ switch($requestPath) {
     break;
     
   default:
-    echo 'Unkown Page' . PHP_EOL;
+    
+    if(contentPageExists($requestPath)){
+      include 'content' . $requestPath . '.php';
+    }
+    else {
+      echo 'Unknown Page' . PHP_EOL; 
+    }
+}
+
+
+function contentPageExists($requestPath) {
+  if(strpos('..', $requestPath) !== FALSE) {
+    return FALSE;
+  }
+  
+  if(file_exists('content' . $requestPath . '.php')){
+    return TRUE;  
+  }
+  return FALSE;
 }
